f7fe990bc5a0461f36c97ee3a8777a2b9c3ced5a
Egg-Fish <eugeniomanansala@gmail.com>
2026-02-16 16:49:24 +0800

    feat: add Stack

--- include/it.h
+++ include/it.h
@@ -14,3 +14,14 @@ void List_appendItem(List l, void *item);
 
 void *List_getItem(List l, size_t index);
 void List_removeItem(List l, size_t index);
+
+typedef List Stack;
+
+Stack Stack_create(void);
+void Stack_destroy(Stack s);
+
+size_t Stack_getLength(Stack s);
+
+void Stack_push(Stack s, void *item);
+void *Stack_peek(Stack s);
+void *Stack_pop(Stack s);
--- src/it.c
+++ src/it.c
@@ -90,3 +90,20 @@ void List_removeItem(List l, size_t index) {
 
     l->length--;
 }
+
+Stack Stack_create(void) { return List_create(); }
+void Stack_destroy(Stack s) { return List_destroy(s); }
+
+size_t Stack_getLength(Stack s) { return List_getLength(s); }
+
+void Stack_push(Stack s, void *item) { List_appendItem(s, item); }
+
+void *Stack_peek(Stack s) { return List_getItem(s, Stack_getLength(s) - 1); }
+
+void *Stack_pop(Stack s) {
+    void *item = Stack_peek(s);
+
+    List_removeItem(s, Stack_getLength(s) - 1);
+
+    return item;
+}
